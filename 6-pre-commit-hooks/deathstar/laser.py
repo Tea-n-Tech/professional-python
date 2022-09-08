3b9ccd4d2789faa5e36fdde2dda2b1c12ecd4851
codie <constantin.diez@gmx.de>
2022-09-08 22:32:17 +0200

    apply fixes

--- 6-pre-commit-hooks/deathstar/laser.py
+++ 6-pre-commit-hooks/deathstar/laser.py
@@ -2,4 +2,10 @@ import rich
 
 
 def fire(planet: str):
+    """Fire the deathstar laser at a planet
+    Parameters
+    ----------
+    planet : str
+        Name of the planet to obliterate.
+    """
     rich.print(f"💥 Firing laster at [red]{planet}[/red]")
